021bd5a357b8e825068405e2bbc60cf1b7897006
Gabriel Gato <70043907+Gato-Gabriel@users.noreply.github.com>
2025-05-16 16:51:02 -0300

    Add distancia entre maquinas

--- algorithm_astar.py
+++ algorithm_astar.py
@@ -1,36 +1,51 @@
 #matriz de adjacencias da academia, com distancias
 grafo = {
-    'Bicicleta': [('Supino', 5), ('C', 5)],
-    'B': [('A', 2), ('D', 4)],
-    'C': [('A', 5)],
-    'D': [('B', 4)]
+    'Esteira': [],
+    'Bicicleta': [('Supino', 9), ('Crossover', 3), ('Scott', 7)],
+    'Cadeira Extensora': [('Supino', 4), ('Scott', 6), ('Crossover', 9)],
+    'Panturrilha': [('Supino', 6), ('Scott',7), ('Crossover',9), ('Banco Reclinável',7)],
+    'Smith': [('Supino',8), ('Scott', 6), ('Crossover',8), ('Banco Reclinável', 4)],
+    'Leg Press': [('Supino', 4), ('Crossover', 5), ('Scott', 4), ('Banco Reclinável', 9)],
+    'Banco Reclinável': [],
+    'Crossover': [],
+    'Supino': [('Bicicleta', 9), ('Leg Press',4), ('Scott',3), ('Cadeira Extensora', 4), ('Smith', 8), ('Panturrilha',7), ('Banco Reclinável',8)],
+    'Scott': [],
 }
 
 energia = {
+    'Esteira': ,
     'Bicicleta': 7,
-    'B': 6,
-    'C': 2,
-    'D': 5,
-    'E': 1,
-    'F': 0
+    'Cadeira Extensora': ,
+    'Panturrilha': ,
+    'Smith': ,
+    'Leg Press': ,
+    'Banco Reclinável': ,
+    'Crossover': ,
+    'Supino': ,
+    'Scott': ,
 }
 
 tempo = {
-    'Bicicleta': 7,
-    'B': 6,
-    'C': 2,
-    'D': 5,
-    'E': 1,
-    'F': 0
+    'Esteira': ,
+    'Bicicleta': 5,
+    'Cadeira Extensora': ,
+    'Panturrilha': ,
+    'Smith': ,
+    'Leg Press': ,
+    'Banco Reclinável': ,
+    'Crossover': ,
+    'Supino': ,
+    'Scott': ,
 }
 
 def personalizar_caminho():
-    estado_inicial = int(input('Qual é o seu estado inicial?'))
-    estado_final = int(input('Qual estado deseja chegar?'))
-    intensidade = float(input('Qual a intensidade do treino? \n0.5 = leve\n1 = moderado \n2 = intenso\n'))
+    estado_inicial = int(input('Qual é o seu equipamento inicial?'))
+    estado_final = int(input('Qual será o equipamento que finalizará seu treino?'))
+    intensidade = float(input('Qual a intensidade do treino? \n0.5 = Leve\n1 = Moderado \n2 = Intenso\n')) #Mudar a qtd de máquinas visitadas antes de chegar na final
     peso_distancia = float(input('Qual a importância da distância entre máquinas? (0 a 1):'))
     peso_energia = float(input('Qual a importância da energia gasta em cada exercício? (0 a 1):'))
     peso_tempo = float(input('Qual a importância do tempo de execução de um exercício? (0 a 1):'))
+    # esses 3 últimos que são float, acho que vale mais a pena pedir de 1 a 10, e depois converter, pra ficar mais intuitivo
 
 
 def main():
